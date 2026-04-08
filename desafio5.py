f0414c458af50b9f6442a3b839ee8060e56b3856
franciscobeckertfreitas-blip <francisco.beckert.freitas@escola.pr.gov.br>
2026-04-07 22:27:27 -0300

    Create desafio5.py

---
+++ desafio5.py
@@ -0,0 +1,11 @@
+# Solicita o valor da base
+base = float(input("Digite a base do triângulo: "))
+
+# Solicita o valor da altura
+altura = float(input("Digite a altura do triângulo: "))
+
+# Calcula a área usando a fórmula (base * altura) / 2
+area = (base * altura) / 2
+
+# Mostra o resultado
+print("A área do triângulo é:", area)
