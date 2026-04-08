b0d53203ad3edb2cf27ffb6429f06d19f140dda4
franciscobeckertfreitas-blip <francisco.beckert.freitas@escola.pr.gov.br>
2026-04-07 22:20:41 -0300

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,11 @@
+# Solicita o primeiro número ao usuário
+num1 = float(input("Digite o primeiro número: "))
+
+# Solicita o segundo número ao usuário
+num2 = float(input("Digite o segundo número: "))
+
+# Soma os dois números digitados
+soma = num1 + num2
+
+# Mostra o resultado da soma na tela
+print("A soma é:", soma)
